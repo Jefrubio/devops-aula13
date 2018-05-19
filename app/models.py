c14a4034ff36204cb488d0f367dd47ac5dd31935
Jefferson Rubio de Oliveira <jerubio@live.com>
2018-05-18 21:27:53 -0300

    Update models.py
    Criação da classe Local_Provas

--- app/models.py
+++ app/models.py
@@ -17,4 +17,11 @@ class Candidato(models.Model):
     nome = models.CharField(max_length=100)
     RG = models.CharField(max_length=9)
     CPF = models.CharField(max_length=11)
+   
+class Local_Provas(models.Model):
+    nome_escola = models.CharField(max_length=500)
+    endereco = models.CharField(max_length=500)
+    cidade = models.CharField(max_length=500)
+    estado = models.CharField(max_length=50)
+    
 
